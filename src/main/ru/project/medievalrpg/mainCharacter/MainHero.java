c08afccbbaba37096e67b7a60b6d85e204ee6fc5
Tonitrus <grom4er@gmail.com>
2020-08-28 00:17:35 +0300

    Test

--- src/main/ru/project/medievalrpg/mainCharacter/MainHero.java
+++ src/main/ru/project/medievalrpg/mainCharacter/MainHero.java
@@ -2,7 +2,7 @@ package ru.project.medievalrpg.mainCharacter;
 
 import ru.project.medievalrpg.actions.Speaking;
 import ru.project.medievalrpg.descriptions.Сharacter;
-
+//test
 
 public class MainHero extends Сharacter implements Speaking {
     public MainHero(String name, int level, int health, int damage, int defence) {
