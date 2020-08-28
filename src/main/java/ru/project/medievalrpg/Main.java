26b7677f0c33f72642712659d22d94a2cc053ad2
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-28 18:16:02 +0300

    version 1

--- src/main/java/ru/project/medievalrpg/Main.java
+++ src/main/java/ru/project/medievalrpg/Main.java
@@ -7,7 +7,6 @@ import ru.project.medievalrpg.mainCharacter.MainHero;
 import java.util.Scanner;
 
 public class Main {
-
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws NullValueException {
