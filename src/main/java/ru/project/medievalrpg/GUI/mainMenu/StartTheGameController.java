76fd3fffad9ce29305abf31a80fb8a639cb652f8
Tonitrus <grom4er@gmail.com>
2020-08-28 17:30:38 +0300

    Test merge to master

---
+++ src/main/java/ru/project/medievalrpg/GUI/mainMenu/StartTheGameController.java
@@ -0,0 +1,26 @@
+package ru.project.medievalrpg.GUI.mainMenu;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+
+public class StartTheGameController {
+    @FXML
+    private Button startGame;
+    @FXML
+    private Button about;
+    @FXML
+    private Button exit;
+    @FXML
+    public void initialize(){
+        startGame.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                startGame.setText("Thanks!");
+            }
+        });
+    }
+
+}
