d31fd8b1e2b926178ebc5e35947222a0df18afc0
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-29 17:39:50 +0300

    add second window with choose name character

---
+++ src/main/java/ru/project/medievalrpg/GUI/mainMenu/ChooseNameController.java
@@ -0,0 +1,14 @@
+package ru.project.medievalrpg.GUI.mainMenu;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ChooseNameController {
+
+    public TextField heroName;
+    @FXML
+    private Button createHero;
+    
+}
