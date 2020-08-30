cb8cdf1f0a8545359ad81cafcab0a286c9776253
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-30 18:27:17 +0300

    add all text in Talk class

--- src/main/java/ru/project/medievalrpg/GUI/mainMenu/SuccessfulRegistrationController.java
+++ src/main/java/ru/project/medievalrpg/GUI/mainMenu/SuccessfulRegistrationController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import ru.project.medievalrpg.Main;
 import ru.project.medievalrpg.NPC.King;
+import ru.project.medievalrpg.dataBase.Talk;
 
 import java.io.IOException;
 
@@ -22,22 +23,14 @@ public class SuccessfulRegistrationController {
     @FXML
     public void initialize(){
 
-        kingText.setText("Мое имя, король " + King.getKingName() + ". Я король этих великих и прекрасных земель.\n" +
-                "Герой! Нашим землям нужен именно ты, не можешь ли ты?...\n" +
-                "Такие слова всегда хочешь услышать в начале приключения.\n" +
-                " Но, вы не благородный герой.\n" +
-                "И давайте начнем ВАШУ историю с самого начала...");
+        kingText.setText(Talk.kingGreeting);
 
         next.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             public void handle(MouseEvent mouseEvent) {
 
                 switch (menuCount){
                     case 0:
-                        kingText.setText("Ты лежишь на столе и в довольно грязном и плохо освещенном помещении\n" +
-                                "Вокруг тебя мерзкий запах. В разных концах комнаты сидят люди.\n" +
-                                "Они такие же как и ты. Нааааркоманы. Тууууунеядцы - Сказали в голове мои бабушки\n" +
-                                "на скамейке. Твои веки медленно открываються. Ты видишь перед собой человека\n" +
-                                "Ты начинаешь вспомнинать... что под тобою что-то мокрое и что твое имя: \n" + Main.mainHero.getName());
+                        kingText.setText(Talk.heroThoughts);
                         menuCount++;
                         break;
 
