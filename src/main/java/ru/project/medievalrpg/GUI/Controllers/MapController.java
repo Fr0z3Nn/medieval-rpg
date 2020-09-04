271b1e996d2e4ae11835981a05b8f14e37e3ede9
Fr0z3Nn <luckyjohn057@gmail.com>
2020-09-05 00:42:10 +0300

    add save and load game

--- src/main/java/ru/project/medievalrpg/GUI/Controllers/MapController.java
+++ src/main/java/ru/project/medievalrpg/GUI/Controllers/MapController.java
@@ -4,6 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import ru.project.medievalrpg.GUI.Start.StartTheGame;
+import ru.project.medievalrpg.Main;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class MapController {
     @FXML
@@ -15,8 +20,21 @@ public class MapController {
     @FXML
     public ImageView ratCave;
     @FXML
+    public ImageView saveGame;
+
+    @FXML
     public void initialize() {
         ratCave.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> StartTheGame.setNewScene("/gui/ratCave.fxml"));
         aboutHero.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> StartTheGame.setNewScene("/gui/aboutHero.fxml"));
+        saveGame.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("SavedGame.ser"));
+                os.writeObject(Main.mainHero);
+                os.close();
+                StartTheGame.setNewScene("/gui/startMenu.fxml");
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+                });
     }
 }
